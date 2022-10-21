8fc2394a632a8a9edfe5658182a564fba30097f3
lusitechmall <hamdanialhassangandi2020@gmail.com>
2022-10-21 22:20:12 +0000

    Logo Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import logo from '../images/memelogo.jpg'
 const Header = () => {
   return (
     <header className='header'>
-      <img src={logo} alt="" className='header--logo' />
+      <img src={logo} alt="logo" className='header--logo' />
       <h1 className='header--title'>Meme Generator</h1>
       <h4 className='lusitech'>LusiTech</h4>
     </header>
