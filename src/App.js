efecdc148137eed242bc21f3869eb6ca70ee2e60
lusitechmall <hamdanialhassangandi2020@gmail.com>
2022-10-21 04:03:11 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import InputFields from './components/InputFields';
+import Overlay from './components/Overlay';
+import PreviewMeme from './components/PreviewMeme';
+import Header from './components/Header'
+
+// Async Function to get all empty Memes available
+async function getPreviewTemplates(setter) {
+  try {
+    const response = await fetch('https://api.memegen.link/templates/');
+    const body = await response.json();
+    setter(body.map((item) => item.blank.split('.png')[0].split('/')[4]));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default function App() {
+  // Store all URLs for preview in an array
+  const [templateNames, setTemplateNames] = useState([]);
+  // Switch between Overlay active and hidden
+  const [overlayHidden, setOverlayHidden] = useState(true);
+  // Save the name of the chosen meme
+  const [chosenMeme, setChosenMeme] = useState('');
+  // Save the top text
+  const [topText, setTopText] = useState('');
+  // Save the bottom text
+  const [bottomText, setBottomText] = useState('');
+  // Save the complete meme URL of the generated meme
+  const [memeUrl, setMemeUrl] = useState('');
+
+  // Use useEffect to setTemplateNames after the fetch is resolved
+  useEffect(() => {
+    getPreviewTemplates(setTemplateNames);
+  }, []);
+
+  return (
+    <div className='App'>
+      <Header />
+      <InputFields
+        setOverlayHidden={setOverlayHidden}
+        topText={topText}
+        setTopText={setTopText}
+        bottomText={bottomText}
+        setBottomText={setBottomText}
+        chosenMeme={chosenMeme}
+        memeUrl={memeUrl}
+        setMemeUrl={setMemeUrl}
+      />
+      <PreviewMeme chosenMeme={chosenMeme} memeUrl={memeUrl} />
+      <Overlay
+        templateNames={templateNames}
+        overlayHidden={overlayHidden}
+        setOverlayHidden={setOverlayHidden}
+        setChosenMeme={setChosenMeme}
+        setMemeUrl={setMemeUrl}
+      />
+    </div>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <h1 className='header--title'>Meme Generator</h1>
+      <h4 className='lusitech'>LusiTech</h4>
+    </header>
+  )
+}
+
+export default Header
